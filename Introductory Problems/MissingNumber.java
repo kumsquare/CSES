f288616bece30683a1697837b88576852d84110f
Kumkum Heralgi <kumkumheragi@gmail.com>
2025-06-21 00:48:45 +0530

    Problem2

--- Introductory Problems/MissingNumber.java
+++ Introductory Problems/MissingNumber.java
@@ -1,4 +1,18 @@
-
+import java.util.Scanner;
 public class MissingNumber {
-
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr[] = new int[n-1];
+        int i;
+        Long actualSum=(n*(n+1L))/2L;
+        Long sum=0L;
+        for(i=0;i<n-1;i++){
+            arr[i]=sc.nextInt();
+            sum=sum+arr[i];
+        }
+        sc.close();
+        Long missingNumber=actualSum-sum;
+        System.out.println(missingNumber);
+    }
 }
